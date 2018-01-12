9e20f419082eb47869d7ea0d1c78ca7e63dbdb26
Damien MacRae <dmacrae99@gmail.com>
2018-01-12 13:02:09 +0800

    Added a help menu

--- src/sbacklight.c
+++ src/sbacklight.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+#include "libbacklight.h"
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		help();
+	}
 
 	return EXIT_SUCCESS;
+}
+
+void help() {
+	printf("sbacklight: Application to control the backlight of laptops\n");
+	printf("USAGE: sbacklight <Option> <Value>\n");
+	printf("<Option>: \n
+			\t-i\t--inc\tIncrease the brightness\n
+			\t-c\t--current\tDisplay the current brightness\n
+			\t-h\t--help\tDisplay this help menu\n");
+	printf("<Value>: This application works in percentage, So use percentage 
+			amounts for functions\n");
 }
